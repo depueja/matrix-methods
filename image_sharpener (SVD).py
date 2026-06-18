f5f628b7d74653316e0f74b6c41aa6e841ef4080
Jordan D <jadepue@outlook.com>
2026-06-17 17:09:35 -0700

    Initial Commit: SVD Image Sharpener with Rank k Approx.

---
+++ image_sharpener (SVD).py
@@ -0,0 +1,38 @@
+#libraries to install. also, install Pillow w/
+# python -m install Pillow
+#directly into terminal
+import numpy as np 
+from numpy.linalg import svd 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#load an image
+image_path = r'C:\projects\learning projects\LEARNING PYTHON\fun projects\greyscale_cat.jpeg'
+im = plt.imread(image_path)
+
+#display the cat!!
+#   plt.imshow(im)
+#   plt.show()
+
+#convert to np array
+a = np.array(im)
+#now, make it greyscale by removing third axis
+b = np.mean(a, 2)
+print(f"original matrix size b4 SVD: {b.shape}")
+#can see shape of image array: print(a.shape)
+
+#perform SVD:
+u, s, vt = np.linalg.svd(b, full_matrices=False)
+
+#now, slice first k singular values to compress image! 
+k = int(input(f"how many singular values would you like to take? you have {s.shape[0]} avaliable. "))
+u1 = u[:,:k]
+s1 = s[:k]
+v1 = vt[0:k,:]
+#recombine decomposed matrices into compressed matrix
+compressed = u1 @ (np.diag(s1) @ v1)
+print(f"matrix size after SVD and recombine: {compressed.shape}.")
+
+plt.imshow(compressed, cmap='grey')
+plt.axis('off')
+plt.show()
