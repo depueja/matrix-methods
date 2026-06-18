027b988e292bccf361180c7c43977458d2b2216a
jordan depue <86802530+depueja@users.noreply.github.com>
2026-06-17 17:11:44 -0700

    Update image_sharpener (SVD).py
    Updated image path placeholder and improved comments.

--- image_sharpener (SVD).py
+++ image_sharpener (SVD).py
@@ -1,22 +1,19 @@
-#libraries to install. also, install Pillow w/
-# python -m install Pillow
-#directly into terminal
 import numpy as np 
 from numpy.linalg import svd 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 #load an image
-image_path = r'C:\projects\learning projects\LEARNING PYTHON\fun projects\greyscale_cat.jpeg'
+image_path = r'INSERT IMAGE HERE'
 im = plt.imread(image_path)
 
-#display the cat!!
+#display the image as check
 #   plt.imshow(im)
 #   plt.show()
 
 #convert to np array
 a = np.array(im)
-#now, make it greyscale by removing third axis
+#now, make it greyscale by removing third axis ("of color")
 b = np.mean(a, 2)
 print(f"original matrix size b4 SVD: {b.shape}")
 #can see shape of image array: print(a.shape)
@@ -24,7 +21,7 @@ print(f"original matrix size b4 SVD: {b.shape}")
 #perform SVD:
 u, s, vt = np.linalg.svd(b, full_matrices=False)
 
-#now, slice first k singular values to compress image! 
+#now, slice first k singular values to compress image
 k = int(input(f"how many singular values would you like to take? you have {s.shape[0]} avaliable. "))
 u1 = u[:,:k]
 s1 = s[:k]
